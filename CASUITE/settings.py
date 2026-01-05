354aa10a7349ff15ee3ea1f1c043337b9e4912b9
Sk Belal Saheb <skbelalsaheb@gmail.com>
2026-01-05 15:30:36 +0530

    Added Docker Changes

--- CASUITE/settings.py
+++ CASUITE/settings.py
@@ -19,8 +19,8 @@ SECRET_KEY = 'django-insecure-i%cb2hzh_ab!ecqqf)g4z)*x^l2qqfnd=d4eo^y3huioo83d1k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ["products-casuite-plaiur-a4a54a-65-21-146-235.traefik.me"]
+ALLOWED_HOSTS = ["https://casuite.mondaltax.com/"]
+CSRF_TRUSTED_ORIGINS = ["https://casuite.mondaltax.com/"]
 
 # Application definition
 
