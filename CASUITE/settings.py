b9e004dc98308bcd0c332d0f5167cff9cdb20e59
Sk Belal Saheb <skbelalsaheb@gmail.com>
2026-01-05 23:22:06 +0530

    Added Docker Changes

--- CASUITE/settings.py
+++ CASUITE/settings.py
@@ -121,12 +121,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
-
-# STATIC_ROOT = BASE_DIR / "static"
-
-# IMPORTANT: If you have your own custom CSS/JS (not just admin files),
-# put them in a folder named 'assets' (or similar) in your project root
-# and uncomment this. Do NOT put source files in 'static' if STATIC_ROOT is also 'static'.
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
