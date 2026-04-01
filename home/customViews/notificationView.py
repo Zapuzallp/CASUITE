2a9152225d6c6472825269b9982f50e929637234
Utkarsh.Tekwani-Zapuza <utkarsh.tekwani_zapuza@yahoo.com>
2026-04-01 15:21:15 +0530

    CASUITE-152  made changes in notifications view and apply leave template

--- home/customViews/notificationView.py
+++ home/customViews/notificationView.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from home.models import Notification
+from home.models import Notification,Leave
 from django.http import JsonResponse
 
 @login_required
